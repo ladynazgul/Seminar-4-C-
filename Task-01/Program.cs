224ffe93aff31991dc082e30bb5169f8946db2f4
ladynazgool <ladynazgool@gmail.com>
2022-09-19 17:24:02 +0400

    Task-01 changed

--- Task-01/Program.cs
+++ Task-01/Program.cs
@@ -1,4 +1,33 @@
 ﻿// Задать массив из 8 элементов и вывести их на экран
 
-int[] array = new int[8];
+// int[] array = new int[] { 0, 1, 2, 3, 4, 5, 6, 7 };
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = new Random().Next(2, 50);
+//     Console.WriteLine("Элемент массива с индексом " + i + " имеет значение " + array[i]);
+// }
+
+// С методами:
+
+int A = 8;
+
+int[] FillArray(int b)
+{
+    int[] arr = new int[b];
+    for (int i = 0; i < b; i++)
+    {
+        arr[i] = new Random().Next(1, 50);
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    Console.Write($"{array[i]} ");
+}
+
+PrintArray(FillArray(A));
+Console.WriteLine();
 
