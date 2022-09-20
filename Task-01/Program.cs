8f524fcf9330b06f96fc93c4ea79ced1091725ac
ladynazgool <ladynazgool@gmail.com>
2022-09-20 15:26:25 +0400

    Task-01 ended

--- Task-01/Program.cs
+++ Task-01/Program.cs
@@ -10,34 +10,50 @@
 
 // С методами:
 
-// int A = 8;
+int A = 8;
 
-// int[] FillArray(int b)
-// {
-//     int[] arr = new int[b];
-//     for (int i = 0; i < b; i++)
-//     {
-//         arr[i] = new Random().Next(1, 50);
-//     }
-//     return arr;
-// }
-
-// void PrintArray(int[] array)
-// {
-//     for (int i = 0; i < array.Length; i++)
-//     Console.Write($"{array[i]} ");
-// }
-
-// PrintArray(FillArray(A));
-// Console.WriteLine();
-
-int[] array = new int[8];
-for (int i = 0; i < 8; i++)
+int[] FillArray(int b)
 {
-	Console.Write($"Введите {i+1} элемент массива: ");
-	array[i] = int.Parse(Console.ReadLine() ?? "0");
+    int[] arr = new int[b];
+    for (int i = 0; i < b; i++)
+    {
+        arr[i] = new Random().Next(0, 10);
+    }
+    return arr;
 }
-Console.Write("Your array: ");
-for (int i = 0; i < array.Length; i++)
-	Console.Write($"{array[i]}  ");
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    Console.Write($"{array[i]} ");
+}
+
+PrintArray(FillArray(A));
+Console.WriteLine();
+
+bool ContainsZero(int[] array)
+{
+	bool answer = false;            // Олег сказал, что это академическое решение, но содержит лишние переменные и операторы, поэтому не очень красивое:)))
+    for (int i = 0; i < array.Length; i++)
+		if (array[i] == 0)
+			answer = true;
+	return answer;	
+}
+
+int[]arr = FillArray(8);
+
+if(ContainsZero(arr) == true)
+	Console.WriteLine("Array contains zeroes");
+else
+	Console.WriteLine("Array doesn't contain zeroes");
+
+// int[] array = new int[8];
+// for (int i = 0; i < 8; i++)
+// {
+// 	Console.Write($"Введите {i+1} элемент массива: ");
+// 	array[i] = int.Parse(Console.ReadLine() ?? "0");
+// }
+// Console.Write("Your array: ");
+// for (int i = 0; i < array.Length; i++)
+// 	Console.Write($"{array[i]}  ");
 
