bd6c5b10d7baa3c6409ec85a726e444c3fae8a4e
ladynazgool <ladynazgool@gmail.com>
2022-09-20 16:52:01 +0400

    Task-04 added

---
+++ Task-04/Program.cs
@@ -0,0 +1,34 @@
+﻿// Написать программу замены элементов массива на противоположные
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 10);
+    }
+}
+
+int[] ReplaceArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = array[i] * (-1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}  ");
+    }
+    Console.WriteLine();
+}
+
+int[] array = new int[8];
+FillArray(array);
+PrintArray(array);
+PrintArray(ReplaceArray(array));
+
+
