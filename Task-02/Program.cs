394eb0575b4650b62f02db5e832bf28ae10fb024
ladynazgool <ladynazgool@gmail.com>
2022-09-20 15:26:48 +0400

    Task-02 added

---
+++ Task-02/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задать массив из 8 элементов, заполненный нулями и единицами, вывести их на экран
+
+int[] EnterArray(int length)
+{
+	int[] array = new int[length];
+	for (int i = 0; i < length; i++)
+		{
+			array[i] = new Random().Next(0,2);
+		}
+		return array;
+}
+
+void PrintArray(int[] array)
+{
+	for (int i = 0; i < array.Length; i++)
+	Console.Write($"{array[i]}  ");
+}
+
+int[] array = EnterArray(8);
+PrintArray(array);
+
