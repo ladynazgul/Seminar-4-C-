2cb338a8f61304c0cb226c1480c3245fbe17f48f
ladynazgool <ladynazgool@gmail.com>
2022-09-21 17:04:41 +0400

    Task-3 added

---
+++ Task-3/Program.cs
@@ -0,0 +1,36 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 200);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}  ");
+    }
+    Console.WriteLine();
+}
+
+int[] array = new int[123];
+FillArray(array);
+PrintArray(array);
+
+int SegmentNumbers(int[] arr)
+{
+    int count = 1;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= 10 && arr[i] <= 99)
+        count++;
+    }
+    return count;
+}
+
+SegmentNumbers(array);
+Console.WriteLine($"Количество чисел из диапазона [10, 99] равно {SegmentNumbers(array)}");
