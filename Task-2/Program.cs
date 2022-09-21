df4a9f9f441d94aa56db088ed32384c67aceb8d7
ladynazgool <ladynazgool@gmail.com>
2022-09-21 16:27:13 +0400

    Task-2 added

---
+++ Task-2/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
+
+int length = 20;
+
+int[] FillArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    Console.Write($"{array[i]} ");
+}
+
+PrintArray(FillArray(length));
+Console.WriteLine();
+
+void FindNumbers(int[] array)
+{
+    int Evens = 0;
+    int Unevens = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        Evens++;
+        else Unevens++;
+    }
+    Console.WriteLine($"В массиве {Evens} четных чисел и {Unevens} нечетных.");
+}
+
+FindNumbers(FillArray(length));
