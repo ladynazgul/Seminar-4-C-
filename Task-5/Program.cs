0a4a0114c074fe06e6888f4149e83e709efe1016
ladynazgool <ladynazgool@gmail.com>
2022-09-22 15:17:11 +0400

    Task-5 and Task-6 added

---
+++ Task-5/Program.cs
@@ -0,0 +1,42 @@
+﻿// Найти произведение пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+// Заполнить массив произвольной длины произвольными числами из заданного диапазона
+
+int length = new Random().Next(4, 30);
+
+int[] FillArray()
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+    }
+    return array;
+}
+
+Console.WriteLine($"Your massiv contains {length} elements");
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write($"{array[i]}  ");
+    Console.WriteLine();
+}
+
+int[] arr = FillArray();
+PrintArray(arr);
+
+void PrintPairProducts(int[] Array)
+{
+    int last = Array.Length - 1;
+    for (int i = 0; i < Array.Length / 2; i++)
+    {
+        Console.WriteLine($"{Array[i]} x {Array[last]} = {Array[i] * Array[last]} ");
+        last--;
+    }
+    if (Array.Length % 2 > 0) Console.WriteLine($"Число {Array[Array.Length / 2]} не имеет пары");
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+PrintPairProducts(arr);
